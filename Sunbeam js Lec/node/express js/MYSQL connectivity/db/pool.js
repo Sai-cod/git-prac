83c2c47dfba14e318b706c32b57fedd23bdb778c
Sairaj <ekbotesairaj640@gmail.com>
2025-12-18 08:43:56 +0530

    deleted db

--- Sunbeam js Lec/node/express js/MYSQL connectivity/db/pool.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2")
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "manager",
-    database: "sunbeam_db"
-})
-
-module.exports = pool
